c29dc489daaa6cf5d70892d507bf47572fc17463
Denis <i@prokaryotes.ru>
2015-11-09 00:53:05 +0100

    add function
    добавление функции блокировки

--- addon/banip.php
+++ addon/banip.php
@@ -5,7 +5,7 @@
  * Version: 1.0.0 (8/Nov/2015)
  * Author: Denis.
  * License: GPL.
- * Site: https://montenegro-it.com
+ * Site: https://montenegro-it.com 
  * Email: contact@montenegro-it.com
  */
 $xml_string = file_get_contents("php://stdin");
@@ -18,16 +18,26 @@ $level = $doc["level"];
 define("PLUGIN_PATH", "/usr/local/ispmgr/var/.plugin_banip/");
 
 class BanIp {
-    
-    static public function create_ban($ip){
-          $ban="iptables -I INPUT -s ".$ip." -j DROP";
-    }
-    static public function delete_ban($ip){
-          $ban="iptables -D INPUT -s ".$ip." -j DROP";
+
+    static public function create_task($ip_array, $unban = 0) {
+        if ($unban) {
+            $flag = "D";
+        } else {
+            $flag = "I";
+        }
+        foreach ($ip_array AS $ip) {
+            $data = "iptables -" . $flag . " INPUT -s " . $ip . " -j DROP";
+            $file = str_replace("/", "_", $ip);
+            file_put_contents(PLUGIN_PATH . $flag . "ban." . $file, $data);
+            if ($unban == 1) {
+                //для удаления флага, если задание не успело отработать
+                @unlink(PLUGIN_PATH . "Iban." . $file);
+            }
+        }
     }
 
     static public function get_listip() {
-        $data = "";
+        $data = array();
         ob_start();
         exec("netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr", $data);
         $return = ob_get_contents();
@@ -48,6 +58,7 @@ class BanIp {
 
     static public function store_cache($ip) {
         $list['hostname'] = htmlspecialchars(gethostbyaddr($ip));
+        //используется собственный сервис определения географии
         $geo = json_decode(file_get_contents('https://ip2geo.link/json/en/' . $ip));
         $list['country'] = $geo->country;
         if (!$list['country']) {
@@ -56,6 +67,63 @@ class BanIp {
         $data = $list['hostname'] . ";" . $list['country'];
         file_put_contents(PLUGIN_PATH . "cache." . $ip, $data);
         return $list;
+    }
+
+    static public function get_rootip() {
+        $data = array();
+        exec('/usr/local/ispmgr/sbin/mgrctl -m ispmgr session -o json', $data);
+        $rows = json_decode(implode("", $data))->elem;
+        $root_ip = array();
+        foreach ($rows AS $row) {
+            if ($row->name == "root") {
+                $root_ip[] = $row->ip;
+            }
+        }
+        return array_unique($root_ip);
+    }
+
+    static public function get_serverip() {
+        $data = array();
+        ob_start();
+        exec('ifconfig |grep -v lo | awk \'/flags/ {printf "Interface "$1" "} /inet/ {printf $2" "} /status/ {printf $2"\n"}\'|grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"', $data);
+        $return = ob_get_contents();
+        ob_end_clean();
+        if ($return == 0) {
+            return array_unique($data);
+        } else {
+            return false;
+        }
+    }
+
+    //убираем из списка на блокировку ip адреса сервера и рута
+    static function clean_ip($ip_array) {
+        $ip_root = self::get_rootip();
+        $ip_server = self::get_serverip();
+        $block_ip = array_diff($ip_array, $ip_root);
+        return array_diff($block_ip, $ip_server);
+    }
+
+    static function clean_subnet($subnet_array) {
+        $subnet_root = array();
+        $subnet_server = array();
+
+        $ip_root = self::get_rootip();
+        $ip_server = self::get_serverip();
+
+        foreach ($ip_root AS $row) {
+            $pos = strrpos($row, '.');
+            $subnet_root[] = substr($row, 0, $pos + 1) . "0/24";
+        }
+        $subnet_root_uniq = array_unique($subnet_root);
+
+        foreach ($ip_server AS $row) {
+            $pos = strrpos($row, '.');
+            $subnet_server[] = substr($row, 0, $pos + 1) . "0/24";
+        }
+        $subnet_server_uniq = array_unique($subnet_server);
+
+        $block_ip = array_diff($subnet_array, $subnet_root_uniq);
+        return array_diff($block_ip, $subnet_server_uniq);
     }
 
     static public function get_countryhostname($ip) {
@@ -70,52 +138,43 @@ class BanIp {
         }
     }
 
+    static public function ban_ip($ip_array, $time, $subnet) {
+        //удаляем из массива адреса текущих сессий рута и адреса сервера
+        $ip_list = self::clean_ip($ip_array);
+        $ip = array();
+        if ($subnet) {
+            foreach ($ip_list AS $row) {
+                $pos = strrpos($row, '.');
+                $ip[] = substr($row, 0, $pos + 1) . "0/24";
+            }
+            //удаляем подсети, если рут или сервер попадает в диапазон
+            $ip_list = self::clean_subnet(array_unique($ip));
+        }
+        self::create_task($ip_list);
+    }
+
 }
 
 switch ($func) {
-    /*
-      case "changepasswd.run";
-      $mysqli = ChangePassword::connect_db();
-      if ($mysqli->connect_errno) {
-      $doc->addChild("changepasswd", "SQL ERROR: " . $mysqli->connect_error);
-      break;
-      }
-      if (!$elid[0]) {
-      $id = $doc->params->user_id[0];
-      } else {
-      $id = $elid[0];
-      }
-      $user_data = ChangePassword::get_userdata($id);
-      $project_data = ChangePassword::get_projectdata($user_data->account);
-      if ($sok == "ok") {
-      if ($user_data->email) {
-      ChangePassword::send_mail($user_data->email, $user_data->name, $user_data->lang, $doc->params->user_password[0], $project_data->name, $project_data->domain, $user_data->realname, $project_data->notifyemail);
-      ChangePassword::update_password($doc->params->user_hash[0], $id);
-      }
-      $doc->addChild("ok", "ok");
-      break;
-      }
-      $password = ChangePassword::generate_password();
-      $salt = ChangePassword::generate_password();
-      $hash = crypt($password, '$1$' . $salt . '$');
-      $doc->addChild("changepasswd", "login: " . $user_data->name . "<br />email: " . $user_data->email . "<br />passwd: " . $password . "<input type=\"hidden\" name=\"user_id\"  value=\"" . $elid . "\"><input type=\"hidden\" name=\"user_hash\"  value=\"" . $hash . "\"><input type=\"hidden\" name=\"user_password\"  value=\"" . $password . "\">");
-      break; */
 
     case "banip.block";
         if ($sok == "ok") {
+            //блокируем
             //$doc->addChild("ok", "ok");
-            $doc->addChild("debug",  json_encode($doc));
+            
+            //BanIp::ban_ip($ip_array, $time, $subnet);
+           
+            $doc->addChild("debug", json_encode($elid));
             break;
         }
-        $doc->addChild("debug",  json_encode($doc));
-       // $doc->addChild("hidden",  json_encode($doc));
-        $time = array(5, 10, 15, 30, 60, 180, 1440);
+        //$doc->addChild("debug", json_encode($doc));
+        // $doc->addChild("hidden",  json_encode($doc));
+        $time = array(5, 10, 15, 30, 60, 180, 1440,'unlimited');
         $slist = $doc->addChild('slist');
         $slist->addAttribute('name', 'time');
         foreach ($time AS $row) {
             $val = $slist->addChild('val', $row);
         }
-       // $doc->addChild("changepasswd", "login: " . $user_data->name . "<br />email: " . $user_data->email . "<br />passwd: " . $password . "<input type=\"hidden\" name=\"user_id\"  value=\"" . $elid . "\"><input type=\"hidden\" name=\"user_hash\"  value=\"" . $hash . "\"><input type=\"hidden\" name=\"user_password\"  value=\"" . $password . "\">");
         break;
     case "banip.stat";
 
@@ -132,9 +191,6 @@ switch ($func) {
             $val = $param->addChild('country', $data['country']);
             $key = $param->addChild('count', $count);
         }
-
-
-
         break;
 
     case "banip.run2";
